fb226fcb6345759ed1fb75686b278ba60ee1563d
Ajwyunsx <107187521+Ajwyunsx@users.noreply.github.com>
2023-08-11 07:25:18 +0800

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -35,7 +35,7 @@ class PauseSubState extends MusicBeatSubstate
 	//var botplayText:FlxText;
 
 	public static var songName:String = '';
-	public static var optionMenu:Bool;
+	public static var wasinsongbeforethenwenttooptions:Bool;
 
 	public function new(x:Float, y:Float)
 	{
