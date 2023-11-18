b2b61905ac43f7eefc08224521c532046309a38f
Ajwyunsx <107187521+Ajwyunsx@users.noreply.github.com>
2023-11-18 18:40:42 +0800

    Update HitboxSettingsSubState.hx

--- source/android/HitboxSettingsSubState.hx
+++ source/android/HitboxSettingsSubState.hx
@@ -38,17 +38,7 @@ class HitboxSettingsSubState extends BaseOptionsMenu
 	{
 		title = 'Hitbox Settings';
 		rpcTitle = 'Hitbox Settings Menu'; //hi, you can ask what is that, i will answer it's all what you needed lol.
-		var option:Option = new Option('Space Shift Extend',
-			"Space and Shift",
-			'hitboxPT',
-			'bool');
-		  addOption(option);
-		var option:Option = new Option('Space Location:',
-			"Choose Space Control Location",
-			'hitboxLocation',
-			'string',
-			['Space', 'Shift', 'Two']);
-		  addOption(option);  
+		
 
 		var option:Option = new Option('Hitbox Mode:',
 			"Choose your Hitbox Style!  -mariomaster",
