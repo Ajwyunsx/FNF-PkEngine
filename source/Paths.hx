53a526c4c1e5411cc6ec0a106bb25397818703e0
Ajwyunsx <107187521+Ajwyunsx@users.noreply.github.com>
2023-12-15 00:06:59 +0800

    Add files via upload

--- source/Paths.hx
+++ source/Paths.hx
@@ -185,6 +185,14 @@ class Paths
 	{
 		return getPath('shaders/$key.vert', TEXT, library);
 	}
+	inline static public function modsShaderFragment(key:String, ?library:String)
+	{
+		return modFolders('shaders/'+key+'.frag');
+	}
+	inline static public function modsShaderVertex(key:String, ?library:String)
+	{
+		return modFolders('shaders/'+key+'.vert');
+	}
 	inline static public function lua(key:String, ?library:String)
 	{
 		return getPath('$key.lua', TEXT, library);
