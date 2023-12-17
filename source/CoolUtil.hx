f58917227c9384f4cf63cc34b632befc3c39293d
Ajwyunsx <107187521+Ajwyunsx@users.noreply.github.com>
2023-12-17 13:14:05 +0800

    Add files via upload

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -1,17 +1,18 @@
 package;
 
-import Paths;
+import flixel.util.FlxSave;
 import flixel.FlxG;
-import flixel.system.FlxSound;
-import haxe.display.Display.Package;
-import haxe.io.Path;
+import openfl.utils.Assets;
+import lime.utils.Assets as LimeAssets;
 import lime.utils.AssetLibrary;
 import lime.utils.AssetManifest;
-import lime.utils.Assets as LimeAssets;
-import openfl.utils.Assets;
-import PlayState;
-import sys.FileSystem;
+import flixel.system.FlxSound;
+#if sys
 import sys.io.File;
+import sys.FileSystem;
+#else
+import openfl.utils.Assets;
+#end
 
 using StringTools;
 
@@ -32,14 +33,13 @@ class CoolUtil
 		trace(snap);
 		return (m / snap);
 	}
-
+	
 	public static function getDifficultyFilePath(num:Null<Int> = null)
 	{
-		if (num == null)
-			num = PlayState.storyDifficulty;
+		if(num == null) num = PlayState.storyDifficulty;
 
 		var fileSuffix:String = difficulties[num];
-		if (fileSuffix != defaultDifficulty)
+		if(fileSuffix != defaultDifficulty)
 		{
 			fileSuffix = '-' + fileSuffix;
 		}
@@ -55,8 +55,7 @@ class CoolUtil
 		return difficulties[PlayState.storyDifficulty].toUpperCase();
 	}
 
-	inline public static function boundTo(value:Float, min:Float, max:Float):Float
-	{
+	inline public static function boundTo(value:Float, min:Float, max:Float):Float {
 		return Math.max(min, Math.min(max, value));
 	}
 
@@ -64,11 +63,9 @@ class CoolUtil
 	{
 		var daList:Array<String> = [];
 		#if sys
-		if (FileSystem.exists(path))
-			daList = File.getContent(path).trim().split('\n');
+		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
 		#else
-		if (Assets.exists(path))
-			daList = Assets.getText(path).trim().split('\n');
+		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
 		#end
 
 		for (i in 0...daList.length)
@@ -78,7 +75,6 @@ class CoolUtil
 
 		return daList;
 	}
-
 	public static function listFromString(string:String):Array<String>
 	{
 		var daList:Array<String> = [];
@@ -91,35 +87,25 @@ class CoolUtil
 
 		return daList;
 	}
-
-	public static function dominantColor(sprite:flixel.FlxSprite):Int
-	{
+	public static function dominantColor(sprite:flixel.FlxSprite):Int{
 		var countByColor:Map<Int, Int> = [];
-		for (col in 0...sprite.frameWidth)
-		{
-			for (row in 0...sprite.frameHeight)
-			{
-				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-				if (colorOfThisPixel != 0)
-				{
-					if (countByColor.exists(colorOfThisPixel))
-					{
-						countByColor[colorOfThisPixel] = countByColor[colorOfThisPixel] + 1;
-					}
-					else if (countByColor[colorOfThisPixel] != 13520687 - (2 * 13520687))
-					{
-						countByColor[colorOfThisPixel] = 1;
-					}
-				}
+		for(col in 0...sprite.frameWidth){
+			for(row in 0...sprite.frameHeight){
+			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+			  if(colorOfThisPixel != 0){
+				  if(countByColor.exists(colorOfThisPixel)){
+				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
+				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
+					 countByColor[colorOfThisPixel] = 1;
+				  }
+			  }
 			}
-		}
+		 }
 		var maxCount = 0;
-		var maxKey:Int = 0; // after the loop this will store the max color
+		var maxKey:Int = 0;//after the loop this will store the max color
 		countByColor[flixel.util.FlxColor.BLACK] = 0;
-		for (key in countByColor.keys())
-		{
-			if (countByColor[key] >= maxCount)
-			{
+			for(key in countByColor.keys()){
+			if(countByColor[key] >= maxCount){
 				maxCount = countByColor[key];
 				maxKey = key;
 			}
@@ -137,26 +123,23 @@ class CoolUtil
 		return dumbArray;
 	}
 
-	// uhhhh does this even work at all? i'm starting to doubt
-	public static function precacheSound(sound:String, ?library:String = null):Void
-	{
+	//uhhhh does this even work at all? i'm starting to doubt
+	public static function precacheSound(sound:String, ?library:String = null):Void {
 		Paths.sound(sound, library);
 	}
 
-	public static function precacheMusic(sound:String, ?library:String = null):Void
-	{
+	public static function precacheMusic(sound:String, ?library:String = null):Void {
 		Paths.music(sound, library);
 	}
 
-	public static function browserLoad(site:String)
-	{
+	public static function browserLoad(site:String) {
 		#if linux
 		Sys.command('/usr/bin/xdg-open', [site]);
 		#else
 		FlxG.openURL(site);
 		#end
 	}
-	
+
 	/** Quick Function to Fix Save Files for Flixel 5
 		if you are making a mod, you are gonna wanna change "ShadowMario" to something else
 		so Base Psych saves won't conflict with yours
@@ -167,59 +150,5 @@ class CoolUtil
 		return #if (flixel < "5.0.0") folder #else FlxG.stage.application.meta.get('company')
 			+ '/'
 			+ FlxSave.validate(FlxG.stage.application.meta.get('file')) #end;
-	}
-}
-
-	public static final formatNotAllowedChars:Array<String> = ["~", "%", "&", ";", ":", '/', '"', "'", "<", ">", "?", "#", " ", "!"];
-
-	public static function formatBindString(str:String):String
-	{
-		var finalStr = str;
-
-		for (notAllowed in formatNotAllowedChars)
-		{
-			finalStr = StringTools.replace(finalStr, notAllowed, "");
-		}
-
-		return finalStr.toLowerCase();
-	}
-
-	public static function findFilesInPath(path:String, extns:Array<String>, ?filePath:Bool = false, ?deepSearch:Bool = true):Array<String>
-	{
-		var files:Array<String> = [];
-
-		if (FileSystem.exists(path))
-		{
-			for (file in FileSystem.readDirectory(path))
-			{
-				var path = haxe.io.Path.join([path, file]);
-				if (!FileSystem.isDirectory(path))
-				{
-					for (extn in extns)
-					{
-						if (file.endsWith(extn))
-						{
-							if (filePath)
-								files.push(path);
-							else
-								files.push(file);
-						}
-					}
-				}
-				else if (deepSearch) // ! YAY !!!! -lunar
-				{
-					var pathsFiles:Array<String> = findFilesInPath(path, extns);
-
-					for (_ in pathsFiles)
-						files.push(_);
-				}
-			}
-		}
-		return files;
-	}
-
-	public static inline function getFileStringFromPath(file:String):String
-	{
-		return Path.withoutDirectory(Path.withoutExtension(file));
 	}
 }
